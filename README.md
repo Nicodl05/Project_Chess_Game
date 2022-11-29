c5a429557251ab5835e305a1018fec16ee465eae
Nicodl05 <78737983+Nicodl05@users.noreply.github.com>
2022-11-29 23:06:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project_Chess_Game
+ Project Chess Game for "Création Numérique ING4"
---
+++ src/main/java/com/example/project_chess_game/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.project_chess_game;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
